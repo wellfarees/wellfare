15df6c26af09781027ef44a3791c6d053412cc11
geenva <geenva@users.noreply.github.com>
2021-08-18 22:40:38 +0100

    prisma support

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
+import { PrismaClient, Prisma } from "@prisma/client";
 import { ApolloServer } from "apollo-server";
 import resolvers from "./resolvers";
 import typeDefs from "./schema";
 
-const server = new ApolloServer({ resolvers, typeDefs });
+class Server extends ApolloServer {
+  public readonly db: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >;
+
+  constructor() {
+    super({
+      resolvers,
+      typeDefs,
+    });
+
+    this.db = new PrismaClient();
+  }
+}
+
+const server = new Server();
 
 export default server;
