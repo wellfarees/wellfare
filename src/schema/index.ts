33ae14ba7516f418c504cf7f3f8bc3d5298481e2
geenva <geenva@users.noreply.github.com>
2021-09-06 19:41:18 +0100

    user field

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -5,6 +5,7 @@
 import { gql } from "apollo-server";
 import company from "./company";
 import ping from "./ping";
+import user from "./user";
 
 const root = gql`
   type Query {
@@ -14,82 +15,6 @@ const root = gql`
   type Mutation {
     root: String
   }
-
-  type Configuration {
-    id: String!
-    darkMode: Boolean!
-    reducedMotion: Boolean!
-    fontSize: Int!
-    User: [User]!
-  }
-
-  type Account {
-    id: String!
-    userId: String!
-    providerType: String!
-    providerId: String!
-    providerAccountId: String!
-    refreshToken: String
-    accessToken: String
-    accessTokenExpires: Float!
-    createdAt: Float!
-    updatedAt: Float!
-    user: User!
-  }
-
-  type Session {
-    id: String!
-    userId: String!
-    expires: Float!
-    sessionToken: String!
-    accessToken: String!
-    createdAt: Float!
-    updatedAt: Float!
-    user: User!
-  }
-
-  type User {
-    id: String!
-    name: String
-    email: String
-    emailVerified: Float
-    image: String
-    createdAt: Float!
-    updatedAt: Float!
-    accounts: [Account]!
-    sessions: [Session]!
-    configurations: Configuration!
-    configurationId: String!
-    affirmations: [String]!
-    record: [Record]!
-    recordId: String!
-  }
-
-  type VerificationRequest {
-    id: String!
-    identifier: String!
-    token: String!
-    expires: Float!
-    createdAt: Float!
-    updatedAt: Float!
-  }
-
-  type Record {
-    id: String!
-    date: Float!
-    description: String!
-    contents: String!
-    emoji: Emoji!
-    emojiId: String!
-    User: [User]!
-  }
-
-  type Emoji {
-    id: String!
-    emoji: String!
-    description: String!
-    Record: [Record]!
-  }
 `;
 
-export default [root, ping, company];
+export default [root, ping, company, user];
