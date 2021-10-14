36587f5028189d677a7375a4792c888f22579c60
Rolands <rolands.affaires@gmail.com>
2021-10-14 18:52:11 +0300

    fixed nex/link import on sidebar comp

--- components/layout/UI/Sidebar.tsx
+++ components/layout/UI/Sidebar.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Link from "next/Link";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSpring, animated, config } from "react-spring";
 import { useEffect, useRef } from "react";
