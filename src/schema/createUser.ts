f4226eaed40bddb252afedd00b89b59e76818807
geenva <geenva@users.noreply.github.com>
2021-11-02 23:31:10 +0000

    implement jwt for createUser

--- src/schema/createUser.ts
+++ src/schema/createUser.ts
@@ -9,6 +9,6 @@ export default gql`
       darkMode: Boolean
       reducedMotion: Boolean
       fontSize: Int
-    ): User
+    ): JWTUser
   }
 `;
---
+++ src/utils/generateJWT.ts
@@ -0,0 +1,7 @@
+import { sign } from "jsonwebtoken";
+
+export default function generateJWT(payload: any) {
+  if (!process.env.JWT_SECRET_KEY)
+    throw new Error("JWT_SECRET_KEY .env variable does not exist.");
+  else return sign(payload, process.env.JWT_SECRET_KEY);
+}
