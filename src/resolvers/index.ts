56a271b0586c0f8c37ff25c27dfac05664cb9783
geenva <geenva@users.noreply.github.com>
2021-11-04 01:06:34 +0000

    login query

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,11 +2,13 @@ import getUser from "./getUser";
 import company from "./company";
 import ping from "./ping";
 import createUser from "./createUser";
+import login from "./login";
 
 const Query = {
   ...ping.Query,
   ...company.Query,
   ...getUser.Query,
+  ...login.Query,
 };
 
 const Mutation = {
