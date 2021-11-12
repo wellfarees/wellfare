431189a9c3dc90d8cd795d0a2c4f451f474711b2
geenva <geenva@users.noreply.github.com>
2021-11-12 20:46:31 +0000

    fix: change affirmations to string

---
+++ prisma/migrations/20211112204119_change_affirmations_from_array_to_steing/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "affirmations" SET NOT NULL,
+ALTER COLUMN "affirmations" SET DATA TYPE TEXT;
---
+++ prisma/migrations/20211112204411_make_affirmations_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "affirmations" DROP NOT NULL;
