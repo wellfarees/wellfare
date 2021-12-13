e83f889967b38a0cc6ef60bf4b76c7cff2ee4293
geenva <geenva@users.noreply.github.com>
2021-12-13 19:59:00 +0000

    return user instead of success in resetPassword

--- src/resolvers/resetPassword.ts
+++ src/resolvers/resetPassword.ts
@@ -22,28 +22,20 @@ export default {
 
       const verificationJWT = generateJWT({ id: data.id }, "password");
       const verificationURL = `${CLIENT_URL}auth/passwordreset?token=${verificationJWT}`;
-      try {
-        await server.mail.send({
-          from: process.env.EMAIL_ADDRESS!,
-          to: data.information.email,
-          subject: "Verify your email",
-          html: `Hi ${data.information.firstName}, here's your password reset link! <a href='${verificationURL}'>Click here</a> to reset your password.
+      server.mail.send({
+        from: process.env.EMAIL_ADDRESS!,
+        to: data.information.email,
+        subject: "Verify your email",
+        html: `Hi ${data.information.firstName}, here's your password reset link! <a href='${verificationURL}'>Click here</a> to reset your password.
             <br /> <br />
             If you cannot click on the URL, please manually paste this into your browser: ${verificationURL}.
             <br /> <br />
             Thanks,
             <br />
             Wellfare`,
-        });
+      });
 
-        return {
-          success: true,
-        };
-      } catch {
-        return {
-          success: false,
-        };
-      }
+      return data;
     },
   },
 };
