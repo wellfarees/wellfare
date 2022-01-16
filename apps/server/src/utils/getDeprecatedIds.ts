b70f619a9eeccc260a5b6e5513b5eb67d404d949
rolandsfr <rolands.affaires@gmail.com>
2022-01-17 00:38:36 +0200

    fix proper deletion of deprecated email changes

--- apps/server/src/utils/getDeprecatedIds.ts
+++ apps/server/src/utils/getDeprecatedIds.ts
@@ -3,6 +3,6 @@ import { EmailsChanged } from "./countRecentEmailsChanged";
 
 export const getDeprecatedIds = (arr: EmailsChanged[]): number[] => {
   return arr
-    .filter((email) => differenceInHours(email.set, Date.now()) > 24)
+    .filter((email) => Math.abs(differenceInHours(email.set, Date.now())) > 24)
     .map((email) => email.id);
 };
