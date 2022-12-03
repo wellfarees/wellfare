afc52b3ef45a609481bf2e1194388ddca5bbb9d5
rolandsfr <rolands.affaires@gmail.com>
2022-12-04 00:01:30 +0200

    edit tsconfig for client

--- apps/server/src/resolvers/oauth/login.ts
+++ apps/server/src/resolvers/oauth/login.ts
@@ -189,6 +189,7 @@ export default {
 
         return { user, publicAlgoliaKey, oAuthRefresh: encoded_refresh };
       } catch (e) {
+        console.log(e);
         return new ApolloError("oAuth failed", "INVALID_OAUTH");
       }
     },
