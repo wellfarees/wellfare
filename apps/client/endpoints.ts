993594c73cae7361ac40cb357b9334fd7035e673
rolandsfr <rolands.affaires@gmail.com>
2022-12-05 20:56:12 +0200

    add deployed server endpoint

--- apps/client/endpoints.ts
+++ apps/client/endpoints.ts
@@ -2,4 +2,4 @@ const development = process.env.NODE_ENV !== "production";
 
 export const SERVER_URL = development
   ? "http://localhost:4000"
-  : "http://localhost:4000";
+  : "https://wellfare-server.onrender.com";
