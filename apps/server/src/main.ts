c92e1eab94f0e7df22977b77d4bf28081006e20e
rolandsfr <rolands.affaires@gmail.com>
2022-12-05 20:37:09 +0200

    change server startup logic

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,6 @@
 import server from "./server";
 
-server.listen().then((url): void =>
+server.listen().then((port: number): void =>
   // eslint-disable-next-line no-console
-  console.log(`[Success] 🚀 Apollo Server is now running at ${url}.`)
+  console.log(`[Success] 🚀 Apollo Server is now running on port ${port}.`)
 );
