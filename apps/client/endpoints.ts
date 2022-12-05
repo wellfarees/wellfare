535edc6c27aed5aa8dec273176794516c41b5c5a
rolandsfr <rolands.affaires@gmail.com>
2022-12-05 18:27:05 +0200

    temporary set up mock server url on client for apollo client

--- apps/client/endpoints.ts
+++ apps/client/endpoints.ts
@@ -1,3 +1,5 @@
 const development = process.env.NODE_ENV !== "production";
 
-export const SERVER_URL = development ? "http://localhost:4000" : "TBD";
+export const SERVER_URL = development
+  ? "http://localhost:4000"
+  : "http://localhost:4000";
