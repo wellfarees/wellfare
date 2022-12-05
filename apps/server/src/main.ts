2385f7679b24abf18098694eea4200ec3c64d965
rolandsfr <rolands.affaires@gmail.com>
2022-12-05 20:49:41 +0200

    fix port type

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,6 @@
 import server from "./server";
 
-server.listen().then((port: number): void =>
+server.listen().then((port): void =>
   // eslint-disable-next-line no-console
   console.log(`[Success] 🚀 Apollo Server is now running on port ${port}.`)
 );
