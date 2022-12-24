9a90c4a03053ba67e4863b35b2b66bcfd5c51c0d
rolandsfr <rolands.affaires@gmail.com>
2022-12-24 18:49:25 +0200

    update url endpoints

--- apps/client/endpoints.ts
+++ apps/client/endpoints.ts
@@ -2,8 +2,8 @@ const development = process.env.NODE_ENV !== "production";
 
 export const SERVER_URL = development
   ? "http://localhost:4000"
-  : "https://wellfare-server.onrender.com";
+  : "https://wellfare-production.up.railway.app";
 
 export const CLIENT_URL = development
   ? "http://localhost:3000"
-  : "https://development.dc2dwwp55mwts.amplifyapp.com";
+  : "https://www.wellfare.space";
