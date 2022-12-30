83d54588718d78061053a55c1a3a1557b5b85601
rolandsfr <rolands.affaires@gmail.com>
2022-12-30 17:29:47 +0200

    update client url

--- apps/server/src/endpoints.ts
+++ apps/server/src/endpoints.ts
@@ -2,4 +2,4 @@ const development = process.env.NODE_ENV !== "production";
 
 export const CLIENT_URL = development
   ? "http://localhost:3000"
-  : "https://wellfare.space";
+  : "https://www.wellfare.space";
