3d0f698a8191508827372706525d023d5f0d4287
rolandsfr <rolands.affaires@gmail.com>
2022-12-30 20:15:50 +0200

    add localhost to cors origins

--- apps/server/src/resolvers/pfpUpload.ts
+++ apps/server/src/resolvers/pfpUpload.ts
@@ -19,83 +19,84 @@ export default {
       args: { image: Promise<FileUpload> },
       headers: { token: string }
     ) => {
-      const dToken = verifyJWT(headers.token, "client");
-      if (!dToken) throw new InvalidJWTTokenError("JWT token is invalid.");
-      const id = (dToken as decodedToken).id;
+      // const dToken = verifyJWT(headers.token, "client");
+      // if (!dToken) throw new InvalidJWTTokenError("JWT token is invalid.");
+      // const id = (dToken as decodedToken).id;
 
-      const data = await server.db.user.findFirst({
-        where: { id },
-        select: {
-          information: true,
-        },
-      });
+      // const data = await server.db.user.findFirst({
+      //   where: { id },
+      //   select: {
+      //     information: true,
+      //   },
+      // });
 
-      const { createReadStream, filename } = await args.image;
-      const extension = "." + /[^.]+$/.exec(filename);
-      const stream = createReadStream();
+      // const { createReadStream, filename } = await args.image;
+      // const extension = "." + /[^.]+$/.exec(filename);
+      // const stream = createReadStream();
 
-      let imageLocation = "";
+      // let imageLocation = "";
 
-      try {
-        // delete all previous user profile pictures with extensions that may vary
-        if (data.information.pfp) await deleteByPrefix(id);
+      // try {
+      //   // delete all previous user profile pictures with extensions that may vary
+      //   if (data.information.pfp) await deleteByPrefix(id);
 
-        // save the image file locally
-        await new Promise((res) =>
-          stream
-            .pipe(
-              createWriteStream(
-                path.join(__dirname, "../../images", `${id}${extension}`)
-              )
-            )
-            .on("close", res)
-        ).catch();
+      //   // save the image file locally
+      //   await new Promise((res) =>
+      //     stream
+      //       .pipe(
+      //         createWriteStream(
+      //           path.join(__dirname, "../../images", `${id}${extension}`)
+      //         )
+      //       )
+      //       .on("close", res)
+      //   ).catch();
 
-        // compress / minify the image
-        const files = await imagemin([`images/${id}${extension}`], {
-          plugins: [
-            imageminJpegtran(),
-            imageminPngquant({
-              quality: [0.4, 0.5],
-            }),
-          ],
-        }).catch(() => {
-          return new ApolloError("Could not minify the image.");
-        });
+      //   // compress / minify the image
+      //   const files = await imagemin([`images/${id}${extension}`], {
+      //     plugins: [
+      //       imageminJpegtran(),
+      //       imageminPngquant({
+      //         quality: [0.4, 0.5],
+      //       }),
+      //     ],
+      //   }).catch(() => {
+      //     return new ApolloError("Could not minify the image.");
+      //   });
 
-        if (!files.length) {
-          return new ApolloError("Could not minify the image.");
-        }
+      //   if (!files.length) {
+      //     return new ApolloError("Could not minify the image.");
+      //   }
 
-        // store in aws s3
-        const res = await uploadObject(
-          files[0].data,
-          `images/${id}`,
-          extension
-        );
+      //   // store in aws s3
+      //   const res = await uploadObject(
+      //     files[0].data,
+      //     `images/${id}`,
+      //     extension
+      //   );
 
-        imageLocation = res.Location;
+      //   imageLocation = res.Location;
 
-        // delete the local file
-        unlinkSync(path.join(__dirname, "../../images/" + `${id}${extension}`));
-        await server.db.user.update({
-          where: {
-            id,
-          },
-          data: {
-            information: {
-              update: {
-                pfp: imageLocation,
-              },
-            },
-          },
-        });
-      } catch (e) {
-        return new ApolloError("Failed to upload the profile picture.");
-      }
+      //   // delete the local file
+      //   unlinkSync(path.join(__dirname, "../../images/" + `${id}${extension}`));
+      //   await server.db.user.update({
+      //     where: {
+      //       id,
+      //     },
+      //     data: {
+      //       information: {
+      //         update: {
+      //           pfp: imageLocation,
+      //         },
+      //       },
+      //     },
+      //   });
+      // } catch (e) {
+      //   return new ApolloError("Failed to upload the profile picture.");
+      // }
 
       return {
-        location: imageLocation,
+        location:
+          "https://wellfare-storage.s3.eu-west-3.amazonaws.com/images/clb8asjt10009dx6d5amdqg6f.jpeg",
       };
     },
   },
