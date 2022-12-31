0c9b7d44591a0a16545545bcd78bd67636e27354
rolandsfr <rolands.affaires@gmail.com>
2022-12-31 02:41:27 +0200

    add favicons

--- apps/client/components/layout/Layout.tsx
+++ apps/client/components/layout/Layout.tsx
@@ -184,6 +184,32 @@ const Layout: React.FC<LayoutProps> = ({ loggedIn, children, isLoaded }) => {
           </>
         )
       ) : null}
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff"></meta>
+        <title>
+          {isLoaded ? (loggedIn ? "Wellfare App" : "Wellfare") : "Wellfare"}
+        </title>
+      </Head>
       <ThemeProvider theme={themes[userInfo.config.theme]}>
         <Modal state={open}>{content}</Modal>
       </ThemeProvider>
