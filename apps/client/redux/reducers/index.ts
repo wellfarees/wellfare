9400b1a7cb3a578e271bc23b1e5ca45033b4c96b
rolandsfr <rolands.affaires@gmail.com>
2023-01-14 18:46:01 +0200

    remove old redux setup

--- apps/client/redux/reducers/index.ts
+++ apps/client/redux/reducers/index.ts
@@ -1,10 +1,8 @@
-import { implementLocalStorage } from "./localStorageReducer";
 import modalReducer from "../actions/modalSlice";
 import unitStateReducer from "../actions/unitStatesSlice";
 import userReduder from "../actions/userSlice";
 
 const reducers = {
-  localStorage: implementLocalStorage,
   unitStates: unitStateReducer,
   modal: modalReducer,
   user: userReduder,
