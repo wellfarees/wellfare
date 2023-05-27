bb98a3d7c6ffaa448ad867bba5d050f55e28d62e
rolandsfr <rolands.affaires@gmail.com>
2023-05-27 21:29:27 +0300

    trying again

--- apps/server/prisma/migrations/20211229190701_make_email_nullable/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Information" ALTER COLUMN "email" DROP NOT NULL;
--- apps/server/prisma/migrations/20220118225650_make_information_id_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[dbid]` on the table `Information` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Information_dbid_key" ON "Information"("dbid");
